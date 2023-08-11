42e488755a7674207ba3c3ddf0b77b4de19f063b
pieroo98 <pippo98siragusa@gmail.com>
2023-08-10 16:56:49 -1200

    schermata settings completata

--- Edustats/programma/screens/Settings.js
+++ Edustats/programma/screens/Settings.js
@@ -1,47 +1,87 @@
 import React, { useState, useEffect } from 'react';
-import { View, TextInput, StyleSheet, Image, Keyboard, ScrollView, Text, TouchableOpacity, Pressable } from 'react-native';
+import { View, TextInput, StyleSheet, Image, Keyboard, ScrollView, Text, Pressable, Switch } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 const Settings = ({ navigation, route }) => {
   const [scorporo, setScorporo] = useState(false);
   const [totalCFU, setTotalCFU] = useState(120);
-  const [tesiCFU, setTesiCFU] = useState(30);
+  const [tesiCFU, setTesiCFU] = useState('');
   const options = [
     { 
-      value: 1,
-      label: "120"
+      key: 1,
+      value: "120"
     },
     {
-      value:  2,
-      label: "180"
+      key:  2,
+      value: "180"
     },
     {
-        value:  3,
-        label: "300"
+        key:  3,
+        value: "300"
     },
     {
-        value:  4,
-        label: "360"
+        key:  4,
+        value: "360"
     }
   ];
 
+  const handle1 = (val) => {
+    let stringa ="";
+    for ( const c1 of options){
+        if (c1.key == val){
+            stringa = c1.value;
+            break;
+        }
+    }
+    setTotalCFU(stringa)
+}
   return (
+    <>
     <ScrollView style={{ backgroundColor: '#fff' }} >
       <View style={{ backgroundColor: '#fff', alignItems: 'center', flex: 1 }}>
         <Image source={require('../assets/tocco_libri.jpg')} style={[styles.image]} />
-            <Text style={styles.testoSignIn} >{'Hello, '+ `${route.params.username}`+'!'} </Text>
-            <Text style={styles.complete} >{'Complete the fields to continue'} </Text>
-            
-            
+        <Text style={styles.testoSignIn} >{'Hello, '+ `${route.params.username}`+'!'} </Text>
+        <Text style={styles.complete} >{'Complete the fields to continue'} </Text>
+        <View style={{ flexDirection: 'row', alignItems: 'center',width:260, height:44, marginBottom:20 }}>
+          <Text style={styles.scorporo}>{"Scorporo"}</Text>
+          <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <Switch trackColor={{ false: "#767577", true: "#1976D2" }} thumbColor={"#f4f3f4"} onValueChange={() => {setScorporo(!scorporo);}} value={scorporo} />
+          </View>
+        </View>
 
+        <SelectList
+          setSelected={(val) => {handle1(val)}}
+          data={options}
+          search={false}
+          maxHeight={156} 
+          boxStyles={{ borderRadius: 40,width:260, height:44 }} //override default styles
+          defaultOption={{ key: options[0].key, value: options[0].value }} />            
+
+        <View style={{ marginTop:20}}>
+          <TextInput
+            placeholder={` Tesi CFU        `}
+            placeholderTextColor='#9E9E9E'
+            keyboardType="numeric"
+            value={tesiCFU}
+            onChangeText={tesiCFU => setTesiCFU(tesiCFU)}
+            onSubmitEditing={() => { Keyboard.dismiss(); }}
+            returnKeyType="send"
+            multiline={false}
+            textAlignVertical='top'
+            style={{ borderWidth: 1, borderRadius: 40, padding:7, color: 'grey', textAlignVertical: 'auto', fontSize:15, fontFamily:'Montserrat-Regular', width:260, height:44 }}
+          />
+        </View>
             
-            <Pressable>
-            <View style={{marginTop:61, borderRadius: 50, width:260, height:44, backgroundColor:'#5271ff', paddingTop: 10}} >
-              <Text style={styles.login} >{'SAVE & CONTINUE'}</Text>
-            </View>
-            </Pressable>
+        
             
       </View>
     </ScrollView>
+    <Pressable style={{ backgroundColor: '#fff', alignItems: 'center' }}>
+    <View style={{marginTop:31,marginBottom:50, borderRadius: 50, width:260, height:44, backgroundColor:'#5271ff', paddingTop: 10}} >
+      <Text style={styles.login} >{'SAVE & CONTINUE'}</Text>
+    </View>
+  </Pressable>
+  </>
   );
 };
 
@@ -76,7 +116,14 @@ const styles = StyleSheet.create({
     textAlign: 'center', 
     fontSize:15, 
     fontFamily:'Montserrat-Regular'
-  }
+  },
+  scorporo:{ 
+    color: 'black', 
+    textAlign: 'left', 
+    fontSize: 16, 
+    textAlignVertical: 'center',
+    fontFamily:'Montserrat-Regular'
+   }
 });
 
 export default Settings;
